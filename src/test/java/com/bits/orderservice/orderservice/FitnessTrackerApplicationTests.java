b03aa897f0486336c85cecb25c0c515e855aa87e
Vivek Pandita <vivek_pandita@rediffmail.com>
2024-11-18 07:30:30 +0530

    Order Service Changes

--- src/test/java/com/bits/orderservice/orderservice/FitnessTrackerApplicationTests.java
+++ src/test/java/com/bits/orderservice/orderservice/FitnessTrackerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.bits.fitnesstracker.fitnesstracker;
+package com.bits.orderservice.orderservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
